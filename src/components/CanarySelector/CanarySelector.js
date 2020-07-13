2e57b6f581280d9de70dcd9a8f75aab2ec38eda2
Rayco Alonso de la Rosa <raycoalonsodelarosa@gmail.com>
2020-07-13 20:28:53 +0100

    Added polyInside function and main data json

--- src/components/CanarySelector/CanarySelector.js
+++ src/components/CanarySelector/CanarySelector.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 //import PropTypes from 'prop-types';
 
+import dataCanarias from './json/canarias.json';
+
 import './CanarySelector.css';
 
 class CanarySelector extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+  }
+
+  polyInside(x, y, poly) {
+    let isInside = false;
+    for (let i = 0, j = poly.length - 1; i < poly.length; j = i++) {
+        let xi = poly[i][0], yi = poly[i][1];
+        let xj = poly[j][0], yj = poly[j][1];
+        let intersect = ((yi > y) !== (yj > y))
+            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) {
+            isInside = !isInside;
+        }
+    }
+    return isInside;
   }
 
   handleClick(event) {
@@ -17,7 +33,13 @@ class CanarySelector extends Component {
     const y = isImgOriginalSize ?
       event.clientY - event.target.offsetTop :
       (event.clientY - event.target.offsetTop) / event.target.clientHeight * 500;
-    console.log(x, y);
+    for (let i = 0; i < dataCanarias.length; i++) {
+      const subData = dataCanarias[i];
+      if (this.polyInside(x, y, subData.poly)) {
+        console.log(subData.label);
+        break;
+      }
+    }
   }
 
   render() {
